15a4ce094d0f134df1b0251e4b7b86f57cf1331e
m-gizzi <matthewgizzi@verizon.net>
2020-02-26 14:19:33 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(visualFlare="*") {
+    return function(input="special") {
+        return `You are ${visualFlare}${input}${visualFlare}!`
+    }
+}
+
+let Calculator = {
+    add: function() {
+        return 1 + 3
+    }, subtract: function() {
+        return 1 - 3
+    }, multiply: function() {
+        return 1 * 3
+    }, divide: function() {
+        return 10 / 5
+    }
+}
+
+function actionApplyer(start, array) {
+    let total = start
+    if (array.length !== 0) {
+        array.forEach(func => {
+            total = func(total)
+        });
+    }
+    return total
+}
