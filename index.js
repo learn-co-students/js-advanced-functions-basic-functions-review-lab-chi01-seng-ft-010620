cc3b61a1d12ca69bfe3d015cc80326fb587d6f5b
Lindsay Mecher <lindsaymecher@gmail.com>
2020-02-29 14:36:25 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function saturdayFun(string = "roller-skate") {
+  return `This Saturday, I want to ${string}!`
+}
+
+function mondayWork(string = "go to the office") {
+  return `This Monday, I will ${string}.`
+}
+
+// function wrapAdjective(flair = "*"){
+//   function innerFunction(param = "special"){
+//     return `You are ${flair}a ${param}${flair}`
+//   }
+//   return innerFunction()
+// }
+
+let wrapAdjective = function(flair="*") {
+  return function(param="special") {
+    return `You are ${flair}${param}${flair}!`
+  }
+}
+
+let Calculator = {
+  add: function(num1, num2){return num1+num2},
+  subtract: function(num1, num2){return num1-num2},
+  multiply: function(num1, num2){return num1*num2},
+  divide: function(num1, num2){return num1/num2}
+}
+
+let actionApplyer = function(int, arrayOfFunctions){
+  let result = int
+  if (arrayOfFunctions.length === 0) {
+    return int
+  } else {
+    arrayOfFunctions.forEach(fn => result = fn(int))
+  }
+  return result -2
+}
